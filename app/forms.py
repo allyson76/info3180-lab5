906bd35fe789bb0613d674c79ab626cc79f1f495
Allison Davis <allison.davis02@mymona.uwi.edu>
2018-03-22 16:45:42 +0000

    final

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, SelectField, TextAreaField
 from wtforms.validators import InputRequired
+from flask_wtf.file import FileField, FileRequired, FileAllowed
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[InputRequired()])
-    password = PasswordField('Password', validators=[InputRequired()])
+    firstname = StringField('First Name', validators=[InputRequired()])
+    lastname = StringField('Last Name', validators=[InputRequired()])
+    gender = SelectField(label='Gender', choices=[("Male", "Male"), ("Female", "Female")])
+    email = StringField('E-mail', validators=[InputRequired()])
+    location = StringField('Location', validators=[InputRequired()])
+    biography = TextAreaField('Biography', validators=[InputRequired()])
+    photo= FileField('Profile Photo', validators=[
+        FileRequired(),
+        FileAllowed(['jpg','png','Images Only!'])
+    ])
